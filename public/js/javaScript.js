8f71c8e8d48208b89e3684af0056fd8b51b7daa6
Gabriel Eugenio Costa <gecn.1812@gmail.com>
2022-07-13 12:46:28 -0300

    Movement teste

---
+++ public/js/javaScript.js
@@ -0,0 +1,12 @@
+window.addEventListener('load', function (){
+
+    const p00 = document.querySelector('#p00').style.margin = '0px 0px 0px 0px'
+    
+    const p0 = document.querySelector('#p0').style.margin = '10px 0px 5px 0px'
+
+    const s0 = document.querySelector('#s0').style.color = '#1fc904'
+
+    const s1 = document.querySelector('#s1').style.color = '#1fc904'
+
+    
+});
